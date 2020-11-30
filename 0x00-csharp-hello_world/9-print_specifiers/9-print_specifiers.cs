a8b519c2b5baec7fb1ece1c59c81dd692208b1e5
modernfuthark <timothys1738@gmail.com>
2020-11-30 17:35:29 -0500

    [N] Added files for task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"Percent: {percent:##.##%}");
+		Console.WriteLine($"Currency: {currency.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-US.utf-8"))}");
+	}
+}
