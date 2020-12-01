348edfc5da9d2f6dc6dcde401762f6541aace5b7
modernfuthark <timothys1738@gmail.com>
2020-12-01 02:56:26 -0500

    [N] didnt say anything about ternary operators :^)

---
+++ 0x01-csharp_ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 99; i++)
+            {
+                Console.Write($"{i:00}{(i != 99 ? ", " : "\n")}");
+            }
+        }
+    }
+}
