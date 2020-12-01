09c823ba018e69ecc15821082fe32085bb1e4ef8
modernfuthark <timothys1738@gmail.com>
2020-12-01 02:36:12 -0500

    [N] Added files for task 2

---
+++ 0x01-csharp_ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i <= 122; i++)
+            {
+                Console.Write((char)i);
+            }
+        }
+    }
+}
