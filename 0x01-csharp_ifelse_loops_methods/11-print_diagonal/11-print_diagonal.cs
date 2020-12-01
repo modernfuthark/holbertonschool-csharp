14e21ce8d8f28fd467248ef21e0806e4bb7d2492
modernfuthark <timothys1738@gmail.com>
2020-12-01 04:12:52 -0500

    [N] Added files for task 11

---
+++ 0x01-csharp_ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write(new string(' ', i) + "\\\n");
+        }
+        Console.Write("\n");
+    }
+}
