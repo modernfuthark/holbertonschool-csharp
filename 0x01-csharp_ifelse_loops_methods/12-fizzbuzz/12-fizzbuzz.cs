42469e7ef02e3fbc8f56515cef7149b54ed5ede3
modernfuthark <timothys1738@gmail.com>
2020-12-01 04:38:37 -0500

    [N] Added files for task 12

---
+++ 0x01-csharp_ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+		static void Main(string[] args)
+        {
+            // Trying a different approach than normal, using a string variable!
+            for (int i = 1; i <= 100; i++)
+            {
+                string res = "";
+                if (i % 3 == 0)
+                    res += "Fizz";
+                if (i % 5 == 0)
+                    res += "Buzz";
+
+                Console.Write($"{(res != "" ? res : i.ToString())}{(i != 100 ? " " : "\n")}");
+            }
+        }
+    }
+}
