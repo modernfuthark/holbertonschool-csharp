baef9fa189aec57dd3b192dbe8f46f92a2fe0e4f
modernfuthark <timothys1738@gmail.com>
2020-12-01 02:39:30 -0500

    [N] Added files for task 3

---
+++ 0x01-csharp_ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i <= 122; i++)
+            {
+                if ((char)i != 'e' && (char)i != 'q')
+                {
+                    Console.Write((char)i);
+                }
+            }
+        }
+    }
+}
