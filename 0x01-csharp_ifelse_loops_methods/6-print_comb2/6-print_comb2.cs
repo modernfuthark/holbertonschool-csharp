7607e709fc5285f14c2121b4d130f48f11a353d1
modernfuthark <timothys1738@gmail.com>
2020-12-01 03:15:11 -0500

    [N] Added files for task 6

---
+++ 0x01-csharp_ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = i + 1; j <= 9; j++)
+                {
+                    Console.Write($"{i}{j}{(i == 8 && j == 9 ? "\n" : ", ")}");
+                }
+            }
+        }
+    }
+}
