c484c8f7b1b0e878ea2f66fe0053196f89eabce1
modernfuthark <timothys1738@gmail.com>
2020-12-01 03:34:23 -0500

    [N] Added files for task 8

---
+++ 0x01-csharp_ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        Console.Write(Math.Abs(number) % 10);
+        return Math.Abs(number) % 10;
+    }
+}
