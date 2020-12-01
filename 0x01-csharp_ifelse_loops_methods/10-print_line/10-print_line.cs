d75f59d57cc58019f60a75e03caa592155839d33
modernfuthark <timothys1738@gmail.com>
2020-12-01 04:06:31 -0500

    [N] Added files for task 10

---
+++ 0x01-csharp_ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+            Console.Write(new string('_', length));
+
+        Console.Write("\n");
+    }
+}
