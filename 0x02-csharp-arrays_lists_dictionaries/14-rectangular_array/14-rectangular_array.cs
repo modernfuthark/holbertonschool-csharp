0769c82e831e97074a19e8e45107ac838b363d30
modernfuthark <timothys1738@gmail.com>
2020-12-02 20:35:17 -0500

    [N] Added files for task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] _new = new int[5,5];
+            _new[2,2] = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    Console.Write($"{_new[i, j]}{(j != 4 ? " " : "\n")}");
+            }
+        }
+    }
+}
