7eba611ee8d35eeb3e4228b56c0bc39ef45ee580
modernfuthark <timothys1738@gmail.com>
2020-12-03 16:05:05 -0500

    [^] Forgot colon in print

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,7 +6,7 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine($"Number of items {aStack.Count}");
+        Console.WriteLine($"Number of items: {aStack.Count}");
 
         Console.WriteLine($"Top item: {aStack.Peek()}");
 
