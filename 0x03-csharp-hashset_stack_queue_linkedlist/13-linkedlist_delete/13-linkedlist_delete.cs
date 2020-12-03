d7d480fd01bf71e4c6ea350ec1007b2c0f984deb
modernfuthark <timothys1738@gmail.com>
2020-12-03 18:18:31 -0500

    [N] Added files for task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int counter = 0;
+        foreach (int element in myLList)
+        {
+            counter++;
+            if (counter == index)
+            {
+                myLList.Remove(element);
+                break;
+            }
+        }
+    }
+}
