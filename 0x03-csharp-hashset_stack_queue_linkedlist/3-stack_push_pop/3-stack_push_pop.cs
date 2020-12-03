9d1b7677d8d100dff90fa274882bdc6ef1393138
modernfuthark <timothys1738@gmail.com>
2020-12-03 16:03:13 -0500

    [N] Added files for task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items {aStack.Count}");
+
+        Console.WriteLine($"Top item: {aStack.Peek()}");
+
+        bool hasItem = aStack.Contains(search);
+        Console.WriteLine($"Stack contains \"{search}\": {hasItem}");
+
+        if (hasItem)
+        {
+            bool memory = false;
+            for (int i = 0; i < aStack.Count; i++)
+            {
+                if (aStack.ElementAt(i) == search)
+                    memory = true;
+
+                aStack.Pop();
+                i--; // Element popped, go down 1 index
+
+                if (memory)
+                    break;
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
