abcaa6d61c1b78e0a74df32f704dfb0b72d072fe
modernfuthark <timothys1738@gmail.com>
2020-12-03 16:57:10 -0500

    [N] Added files for task 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        foreach (var element in myLList)
+            count++;
+        return count;
+    }
+}
