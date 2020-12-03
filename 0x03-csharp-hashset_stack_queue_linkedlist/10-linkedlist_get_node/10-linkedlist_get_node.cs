5b6cfff0e14facde6f117c420e22aa4ebd13d9d7
modernfuthark <timothys1738@gmail.com>
2020-12-03 17:35:26 -0500

    [^] Updated method to be cleaner

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -6,12 +6,13 @@ class LList
 {
     public static int GetNode(LinkedList<int> myLList, int n)
     {
-        if (n >= myLList.Count)
-            return 0;
         int index = 0;
         foreach (int ele in myLList)
-            if (index != n)
-                index++;
-        return myLList.ElementAt(index);
+        {
+            if (index == n)
+                return ele;
+            index++;
+        }
+        return 0;
     }
 }
