213ef9721a52bd414f43d9168073707bdd0528c9
modernfuthark <timothys1738@gmail.com>
2020-12-02 20:22:20 -0500

    [+] Added a check for the count of myList

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -5,6 +5,9 @@ class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
+        if (myList.Count < 1)
+            return "None";
+
         KeyValuePair<string, int> top = new KeyValuePair<string, int>("None", 0);
 
         foreach (KeyValuePair<string, int> v in myList)
