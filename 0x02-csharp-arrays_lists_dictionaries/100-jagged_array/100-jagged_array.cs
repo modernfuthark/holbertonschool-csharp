3e41dc6ba2f2f9bf4223bb068fda88c8671cfb0d
modernfuthark <timothys1738@gmail.com>
2020-12-02 20:53:38 -0500

    [N] Added files for task 16

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged = {
+                new int[] {0, 1, 2, 3},
+                new int[] {0, 1, 2, 3, 4, 5, 6},
+                new int[] {0, 1}
+            };
+
+            for (int i = 0; i < jagged.Length; i++)
+           {
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    Console.Write($"{jagged[i][j]}{(j != jagged[i].Length - 1 ? " " : "\n")}");
+                }
+            }
+        }
+    }
+}
