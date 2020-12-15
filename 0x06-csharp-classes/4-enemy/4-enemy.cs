17ecd44032f84b2a61a6d148e276df330ebefb65
modernfuthark <timothys1738@gmail.com>
2020-12-15 01:41:12 -0500

    [N] Added files for task 4

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class for a zombie
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// The zombie's health
+        /// </summary>
+        private int health;
+
+        /// <summary>The zombie's name</summary>
+        private string name = "(No name)";
+        /// <summary>Getter and setter of zombie's name</summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        /// <summary>
+        /// Class constructor for <see cref="Zombie"/>
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Class constructor for <see cref="Zombie"/> with a health value
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                this.health = value;
+            else
+                throw new ArgumentException("Health must be greater than or equal to 0");
+        }
+
+        /// <summary>
+        /// Getter for the health field
+        /// </summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
