3f4ee6b8f7272340b53488dd1a090a3f50118649
modernfuthark <timothys1738@gmail.com>
2020-12-15 01:22:17 -0500

    [N] Added files for task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class for a zombie
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// The zombie's health
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Class constructor for <see cref="Zombie"/>
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
