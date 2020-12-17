da36333f5b658169ca69801d0b2b9df815cbca64
modernfuthark <timothys1738@gmail.com>
2020-12-17 02:42:23 -0500

    [N] Added base files for task 0

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Class containing various math operations
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Adds 2 numbers together
+        /// </summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
