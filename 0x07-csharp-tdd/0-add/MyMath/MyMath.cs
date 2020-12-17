d2408407f4a33213c64d2e8190c4d9ad69bf6ba3
modernfuthark <timothys1738@gmail.com>
2020-12-17 03:12:36 -0500

    [N] Added missing <returns> XML documentation

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -10,6 +10,7 @@ namespace MyMath
         /// <summary>
         /// Adds 2 numbers together
         /// </summary>
+        /// <returns> Sum of 2 given integers </returns>
         public static int Add(int a, int b)
         {
             return (a + b);
