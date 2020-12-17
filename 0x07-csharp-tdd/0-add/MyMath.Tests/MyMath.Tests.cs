3c4be7d39a51a718b562e6edb1aa6722749c77d9
modernfuthark <timothys1738@gmail.com>
2020-12-17 03:04:16 -0500

    [^] Added tests

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -2,17 +2,27 @@ using NUnit.Framework;
 
 namespace MyMath.Tests
 {
-    public class Tests
+    /// <summary>
+    /// NUnit Tests for the Operations class
+    /// </summary>
+    public class MathTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Add_TwoInts(
+            [Values(1, 2, 3, 4, 5, -6, -5000, 5300, 22222)] int a,
+            [Values(0, -1, -2, 12000, 5554, 30, 75, -29)] int b)
         {
+            var testAdd = Operations.Add(a, b);
+
+            Assert.That(testAdd == (a + b));
         }
 
         [Test]
-        public void Test1()
+        public void Add_MinMax()
         {
-            Assert.Pass();
+            var testAdd = Operations.Add(int.MinValue, int.MaxValue);
+
+            Assert.That(testAdd == -1);
         }
     }
 }
