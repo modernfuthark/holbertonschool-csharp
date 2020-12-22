503fef7fdbde68c4430d472503354628cb7c96f6
modernfuthark <timothys1738@gmail.com>
2020-12-21 19:51:17 -0500

    [N] Added files for task 8

---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,69 @@
+using System;
+
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width {
+        get => width;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+
+    public int Height {
+        get => height;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+
+    public new int Area()
+    {
+        return (this.height * this.width);
+    }
+
+	public override string ToString()
+	{
+		return $"[Rectangle] {this.width} / {this.height}";
+	}
+}
+
+
+class Square : Rectangle
+{
+    private int size;
+
+    public int Size {
+        get => size;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            else
+            {
+                size = value;
+                this.Width = value;
+                this.Height = value;
+            }
+        }
+    }
+}
