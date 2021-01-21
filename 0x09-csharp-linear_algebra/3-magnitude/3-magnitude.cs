93050c4a09830adf1187d3c8bc2bf7f2a82cf9ab
modernfuthark <timothys1738@gmail.com>
2021-01-20 22:58:54 -0500

    [N] Added files to directory

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Various vector calculations
+/// </summary>
+class VectorMath
+{
+	/// <summary>
+	/// Calculates length of a given vector, 2D or 3D
+	/// </summary>
+	public static double Magnitude(double[] vector)
+	{
+		if (vector.Length != 2 && vector.Length != 3)
+			return (-1);
+
+        double sum = 0;
+
+        foreach (var v in vector)
+            sum += (Math.Pow(v, 2));
+
+        return Math.Round(Math.Sqrt(sum), 2);
+	}
+}
