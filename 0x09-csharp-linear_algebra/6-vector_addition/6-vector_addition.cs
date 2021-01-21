ed520fa3be4bfee50284339672f2ae64da6085f0
modernfuthark <timothys1738@gmail.com>
2021-01-20 23:36:17 -0500

    [+] Added XML documentation

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,7 +1,13 @@
 using System;
 
+/// <summary>
+/// Various vector calculations
+/// </summary>
 class VectorMath
 {
+    /// <summary>
+    /// Calculates the sum of a given vector, 2D or 3D
+    /// </summary>
     public static double[] Add(double[] vector1, double[] vector2)
     {
         if ((vector1.Length == 2 || vector1.Length == 3) && (vector2.Length == 2 || vector2.Length == 3))
