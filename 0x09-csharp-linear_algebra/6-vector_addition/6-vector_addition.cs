15a9e82084c2c7816a969f241bf4ae690bd93def
modernfuthark <timothys1738@gmail.com>
2021-01-20 23:42:39 -0500

    [^] Added check for vectors being the same length

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -10,11 +10,11 @@ class VectorMath
     /// </summary>
     public static double[] Add(double[] vector1, double[] vector2)
     {
-        if ((vector1.Length == 2 || vector1.Length == 3) && (vector2.Length == 2 || vector2.Length == 3))
+        if ((vector1.Length == 2 || vector1.Length == 3) && vector1.Length == vector2.Length)
         {
             for (int i = 0; i < vector1.Length; i++)
                 vector1[i] += vector2[i];
-
+ 
             return (vector1);
         }
 
