e2b1498a8d639bc9ae8d3bbb2906aba8b41718e0
modernfuthark <timothys1738@gmail.com>
2021-01-21 19:28:50 -0500

    [N] Added files

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,21 @@
+using System;
+
+/// <summary>
+/// Contains various matrix calculations
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Calcualtes a scalar multiplication of a matrix
+    /// </summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.GetLength(0) != matrix.GetLength(1) && (matrix.GetLength(0) != 2 || matrix.GetLength(0) != 3))
+            return (new double[,] {{-1}});
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                matrix[i, j] *= scalar;
+        return (matrix);
+    }
+}
