2aac9c5524d8e6177b24cdfadff73da5dacba4e6
modernfuthark <timothys1738@gmail.com>
2021-03-23 22:45:24 -0400

    [N] Added files for task 1 to directory

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,81 @@
+using System;
+
+/// <summary>
+/// Base class
+/// </summary>
+class Base
+{
+    public string name {get; set;}
+
+    /// <summary>
+    /// ToString override
+    /// </summary>
+	public override string ToString()
+	{
+		return $"{name} is a {this.GetType()}";
+	}
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+	public int durability {get; set;}
+	public bool isCollected {get; set;}
+
+	public void Interact()
+	{
+		
+	}
+
+	public void Break()
+	{
+		
+	}
+
+	public void Collect()
+	{
+
+	}
+}
+
+/// <summary>
+/// Interactive interface
+/// </summary>
+public interface IInteractive
+{
+	/// <summary>
+	/// Interact method
+	/// </summary>
+	void Interact();
+}
+
+/// <summary>
+/// Breakable interface
+/// </summary>
+public interface IBreakable
+{
+	/// <summary>
+	/// Durability property
+	/// </summary>
+	int durability {get; set;}
+
+	/// <summary>
+	/// Break method
+	/// </summary>
+	void Break();
+}
+
+/// <summary>
+/// Collectable interface
+/// </summary>
+public interface ICollectable
+{
+	/// <summary>
+	/// isCollected property
+	/// </summary>
+	bool isCollected {get; set;}
+
+	/// <summary>
+	/// Collect method
+	/// </summary>
+	void Collect();
+}
