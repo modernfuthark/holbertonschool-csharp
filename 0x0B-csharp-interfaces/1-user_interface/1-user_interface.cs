60c7d335ba7f3ea430e9e04aa7c352a2782abcd3
modernfuthark <timothys1738@gmail.com>
2021-03-23 23:01:50 -0400

    [^] Changed around public keywords

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -18,67 +18,68 @@ public abstract class Base
 		return $"{name} is a {this.GetType()}";
 	}
 }
-
-class TestObject : Base, IInteractive, IBreakable, ICollectable
+// Interactive interface
+interface IInteractive
 {
+	// Interact method
+	void Interact();
+}
+
+// Breakable interface
+interface IBreakable
+{
+	// Durability amount
+	int durability {get; set;}
+
+	// Break method
+	void Break();
+}
+
+// Collectable interface
+interface ICollectable
+{
+	// Collected bool
+	bool isCollected {get; set;}
+
+	// Collect method
+	void Collect();
+}
+
+/// <summary>
+/// TestObject class
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+	/// <summary>
+	/// Durability property
+	/// </summary>
 	public int durability {get; set;}
+	/// <summary>
+	/// isCollected durability
+	/// </summary>
 	public bool isCollected {get; set;}
 
+	/// <summary>
+	/// Interact method
+	/// </summary>
 	public void Interact()
 	{
 
 	}
 
+	/// <summary>
+	/// Break method
+	/// </summary>
 	public void Break()
 	{
 
 	}
 
+	/// <summary>
+	/// Collect method
+	/// </summary>
 	public void Collect()
 	{
 
 	}
-}
-
-/// <summary>
-/// Interactive interface
-/// </summary>
-public interface IInteractive
-{
-	/// <summary>
-	/// Interact method
-	/// </summary>
-	void Interact();
-}
-
-/// <summary>
-/// Breakable interface
-/// </summary>
-public interface IBreakable
-{
-	/// <summary>
-	/// Durability property
-	/// </summary>
-	int durability {get; set;}
-
-	/// <summary>
-	/// Break method
-	/// </summary>
-	void Break();
-}
-
-/// <summary>
-/// Collectable interface
-/// </summary>
-public interface ICollectable
-{
-	/// <summary>
-	/// isCollected property
-	/// </summary>
-	bool isCollected {get; set;}
-
-	/// <summary>
-	/// Collect method
-	/// </summary>
-	void Collect();
 }
