86f19b3ab4a0fedb4147460e569d5c0ada85b1b7
modernfuthark <timothys1738@gmail.com>
2021-03-25 18:00:42 -0400

    [N] Added files for task 0

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,34 @@
+using System;
+
+/// <summary>
+/// Player class
+/// </summary>
+public class Player
+{
+    private string name {get; set;}
+    private float maxHp {get; set;}
+    private float hp {get; set;}
+
+    /// <summary>
+    /// Class constructor
+    /// </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0) {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default");
+            this.maxHp = 100f;
+        } else {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Method to print health of instance
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
