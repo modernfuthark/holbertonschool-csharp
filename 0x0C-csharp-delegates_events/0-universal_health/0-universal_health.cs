0185bc0b3a75e2ed30daca0b8b2b89dc0786f958
modernfuthark <timothys1738@gmail.com>
2021-03-25 18:07:36 -0400

    [+] Added period to low hp assignment message

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -15,7 +15,7 @@ public class Player
     public Player(string name = "Player", float maxHp = 100f)
     {
         if (maxHp <= 0) {
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default");
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
             maxHp = 100f;
         }
         this.maxHp = maxHp;
