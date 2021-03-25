2b5ff2baf63659d45ac8322e53ffbacd143f1dd8
modernfuthark <timothys1738@gmail.com>
2021-03-25 18:01:24 -0400

    [-] Removed unneeded else statement

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -16,10 +16,8 @@ public class Player
     {
         if (maxHp <= 0) {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default");
-            this.maxHp = 100f;
-        } else {
-            this.maxHp = maxHp;
         }
+        this.maxHp = maxHp;
         this.name = name;
         this.hp = this.maxHp;
     }
